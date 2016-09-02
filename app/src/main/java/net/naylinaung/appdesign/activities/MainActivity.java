1288d4fbcb91428bf751c9059a9273296c1a263d
SteveNay <naylinaung.softwarearchitect@gmail.com>
2016-09-02 16:00:46 +0630

    Toolbar Entry Animation

--- app/src/main/java/net/naylinaung/appdesign/activities/MainActivity.java
+++ app/src/main/java/net/naylinaung/appdesign/activities/MainActivity.java
@@ -1,24 +1,50 @@
 package net.naylinaung.appdesign.activities;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.OvershootInterpolator;
+import android.widget.Toast;
 
 import net.naylinaung.appdesign.R;
+import net.naylinaung.appdesign.adapters.MyCourseAdapter;
+import net.naylinaung.appdesign.utils.ScreenUtils;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+
+    @BindView(R.id.content)
+    CoordinatorLayout clContent;
+
+    @BindView(R.id.rvMyCourse)
+    RecyclerView rvMyCourse;
+
+    @BindView(R.id.fab_search)
+    FloatingActionButton fabSearch;
+
+    public static final String ACTION_SHOW_LOADING_ITEM = "action_show_loading_item";
+    public static final int ACTION_BAR_SIZE = ScreenUtils.dpToPx(56);
+
+    private static final int ANIM_DURATION_TOOLBAR = 300;
+    private static final int ANIM_DURATION_FAB = 400;
+
+    private MyCourseAdapter myCourseAdapter;
+
+    private boolean pendingIntroAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,12 +61,89 @@ public class MainActivity extends AppCompatActivity {
             actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
+        setupFeed();
+
+        if (savedInstanceState == null) {
+            pendingIntroAnimation = true;
+            prepareIntroAnimation();
+        } else {
+            myCourseAdapter.updateItems(false);
+        }
+
+    }
+
+    private void setupFeed() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this) {
+            @Override
+            protected int getExtraLayoutSpace(RecyclerView.State state) {
+                return 300;
+            }
+        };
+        rvMyCourse.setLayoutManager(linearLayoutManager);
+
+        myCourseAdapter = new MyCourseAdapter(this);
+//        myCourseAdapter.setOnFeedItemClickListener(this);
+        rvMyCourse.setAdapter(myCourseAdapter);
+        rvMyCourse.setOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+//                FeedContextMenuManager.getInstance().onScrolled(recyclerView, dx, dy);
+            }
+        });
+    }
+
+    private void prepareIntroAnimation() {
+        getToolbar().setTranslationY(-ACTION_BAR_SIZE);
+        getScreenTitle().setTranslationY(-ACTION_BAR_SIZE);
+
+    }
+
+    private void startIntroAnimation() {
+        fabSearch.setTranslationY(2 * getResources().getDimensionPixelOffset(R.dimen.btn_fab_size));
+        getInboxMenuItem().getActionView().setTranslationY(-ACTION_BAR_SIZE);
+
+        getToolbar().animate()
+                .translationY(0)
+                .setDuration(ANIM_DURATION_TOOLBAR)
+                .setStartDelay(300);
+        getScreenTitle().animate()
+                .translationY(0)
+                .setDuration(ANIM_DURATION_TOOLBAR)
+                .setStartDelay(400);
+        getInboxMenuItem().getActionView().animate()
+                .translationY(0)
+                .setDuration(ANIM_DURATION_TOOLBAR)
+                .setStartDelay(500)
+                .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        startContentAnimation();
+                    }
+                })
+                .start();
+    }
+
+    private void startContentAnimation() {
+        fabSearch.animate()
+                .translationY(0)
+                .setInterpolator(new OvershootInterpolator(1.f))
+                .setStartDelay(300)
+                .setDuration(ANIM_DURATION_FAB)
+                .start();
+
+//        myCourseAdapter.updateItems(true);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        super.onCreateOptionsMenu(menu);
+
+        if (pendingIntroAnimation) {
+            pendingIntroAnimation = false;
+            startIntroAnimation();
+        }
+
         return true;
     }
 
@@ -53,9 +156,14 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_profile) {
+            Toast.makeText(getApplicationContext(), "Tap on Profile", Toast.LENGTH_LONG).show();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void showLikedSnackbar() {
+        Snackbar.make(clContent, "Liked!", Snackbar.LENGTH_SHORT).show();
     }
 }
